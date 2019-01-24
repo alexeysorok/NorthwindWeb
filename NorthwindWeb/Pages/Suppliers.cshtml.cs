d4a0c682d0f5dafe2f670f60ec34fda6c0fa88e9
alexe <alexe@W10SSD>
2019-01-24 13:52:10 +0300

    Change Suppliers

--- NorthwindWeb/Pages/Suppliers.cshtml.cs
+++ NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -9,8 +9,13 @@ namespace NorthwindWeb.Pages
 {
     public class SuppliersModel : PageModel
     {
+        public IEnumerable<string> Suppliers { get; set; }
+
         public void OnGet()
         {
+            ViewData["Title"] = "Northwind Web Site - Suppliers";
+            Suppliers = new[]
+            { "Alpha Co", "Beta Limited", "Gamma Corp" };
 
         }
     }
