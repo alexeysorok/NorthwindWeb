9de908a6e851e1c4f3ded1dab20d6c548083ea31
alexe <alexe@W10SSD>
2019-01-24 15:35:42 +0300

    Исправление: ошибка в кавычках

--- NorthwindEntitiesLib/Category.cs
+++ NorthwindEntitiesLib/Category.cs
@@ -5,11 +5,11 @@ using System.Text;
 namespace Packt.CS7
 {
     public class Category
-        {
-            public int CategoryID { get; set; }
-            public string CategoryName { get; set; }
-            public string Description { get; set; }
-            public ICollection<Product> Products { get; set; }
-        }
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
+    
 }
