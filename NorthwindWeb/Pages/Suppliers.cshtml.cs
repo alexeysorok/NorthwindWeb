b01d9e33e67ea46aef3024df65c891f0a8d12514
alexe <alexe@W10SSD>
2019-01-24 15:50:42 +0300

    Выводим данные из базы данных

--- NorthwindWeb/Pages/Suppliers.cshtml.cs
+++ NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -4,18 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.CS7;
+
+
 
 namespace NorthwindWeb.Pages
 {
     public class SuppliersModel : PageModel
     {
+        private Northwind db;
+        public SuppliersModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+
         public IEnumerable<string> Suppliers { get; set; }
 
         public void OnGet()
         {
             ViewData["Title"] = "Northwind Web Site - Suppliers";
-            Suppliers = new[]
-            { "Alpha Co", "Beta Limited", "Gamma Corp" };
+            //Suppliers = new[]
+            //{ "Alpha Co", "Beta Limited", "Gamma Corp" };
+            Suppliers = db.Suppliers.Select(s => s.CompanyName).ToArray();
+
 
         }
     }
