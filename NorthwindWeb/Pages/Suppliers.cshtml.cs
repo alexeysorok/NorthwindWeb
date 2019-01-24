99963c325455ca7c49ec2caf352c951f70f3de8e
alexe <alexe@W10SSD>
2019-01-24 13:40:37 +0300

    add page Suppliers

---
+++ NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthwindWeb.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
