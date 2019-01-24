2bbfd0395a0a208117f047b7bdbd0188799eb50d
alexe <alexe@W10SSD>
2019-01-24 15:31:03 +0300

    Добавить библиотеки для работы с базой данных

---
+++ NorthwindEntitiesLib/Shipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packt.CS7
+{
+    public class Shipper
+    {
+
+        public int ShipperID { get; set; }
+        public string ShipperName { get; set; }
+        public string Phone { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
